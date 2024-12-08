a3ec75d4c33c13277f9e965f9e96162eb1500864
ponakanjibrill <161431996+ponakanjibrill@users.noreply.github.com>
2024-12-09 04:24:11 +0700

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,6 +1,6 @@
 import os
 import base64
-from solana.keypair import Keypair  # Updated import
+from solana.keypair import Keypair
 from solana.rpc.api import Client
 from solana.transaction import Transaction
 from solana.system_program import TransferParams, transfer
